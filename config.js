57403c6fec1269130f5866fcc9babac8290089ba
SIYOUNG <siyeong5034@gmail.com>
2026-06-01 10:24:34 +0900

    Update config.js

--- config.js
+++ config.js
@@ -72,8 +72,8 @@ const CONFIG = {
     ],
     bride: [
       { role: "신부", name: "손 지영", bank: "하나은행", number: "1002-481-960619" },
-      { role: "아버지", name: "손 영만", bank: "제일은행", number: "203-20-186290" },
-      { role: "어머니", name: "박 현리", bank: "우리은행", number: "559-153231-02006" }
+      { role: "아버지", name: "손 영만", bank: "우리은행", number: "559-153231-02006" },
+      { role: "어머니", name: "박 현리", bank: "제일은행", number: "203-20-186290" }
     ]
   },
 
